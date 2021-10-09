efefba8fc7c0b0dfcfb3d4cbf9acb2b446d1969f
Bahodir8586 <bahodira213@gmail.com>
2021-10-09 18:06:04 +0500

    create task done

--- components/CreateTask/createTask.jsx
+++ components/CreateTask/createTask.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const CreateTask = ({createTask}) => {
+    const [name, setName] = useState("")
     return (
-        <div className={"text-center w-96 mx-auto"}>
-            <label htmlFor="email" className="block text-2xl font-medium text-gray-700">
-                New Task
-            </label>
-            <div className="mt-2">
-                <input
-                    type="text"
-                    name="task"
-                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                    placeholder="New Task..."
-                />
-            </div>
+        <div className={"text-center mx-auto"}>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                createTask(name)
+            }}>
+                <label htmlFor="email" className="block text-2xl font-medium text-gray-700">
+                    New Task
+                </label>
+                <div className="mt-2 ">
+                    <input
+                        type="text"
+                        name="task"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="shadow-sm w-96 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md py-2"
+                        placeholder="New Task..."
+                    />
+                    <button type="button" onClick={() => createTask(name)}
+                            className="px-4 py-2 text-base border rounded-lg bg-green-500 text-white hover:bg-green-700 ">
+                        Create
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
