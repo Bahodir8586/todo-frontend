c932887c7e03b0ef2a68926020bedaa64c6b937c
Bahodir8586 <bahodira213@gmail.com>
2021-10-12 18:11:05 +0500

    reset password functionality fully finished

--- pages/resetPassword/[token].js
+++ pages/resetPassword/[token].js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {useRouter} from "next/router";
 import ResetPasswordForm from "../../components/ResetPassword";
 import axios from "../../components/axios";
+import Cookies from "js-cookie";
 
 export async function getServerSideProps(context) {
     const jwt = context.req.headers.cookie?.split(';')[1]?.split('=')[1]
@@ -24,6 +25,8 @@ const ResetPassword = () => {
         console.log(token, password, passwordConfirm)
         axios.patch(`/users/resetPassword/${token}`, {password, passwordConfirm}).then((response) => {
             console.log(response)
+            Cookies.set('token', response.data.token)
+            router.push('/')
         }).catch(error => {
             setErrorMessage(error.response?.data?.message)
         })
