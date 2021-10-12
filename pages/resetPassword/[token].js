4cf612ca7b06c2724fc353127e0de4e34738f4ae
Bahodir8586 <bahodira213@gmail.com>
2021-10-12 16:43:12 +0500

    blocked new pages

--- pages/resetPassword/[token].js
+++ pages/resetPassword/[token].js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+export async function getServerSideProps(context) {
+    const jwt = context.req.headers.cookie?.split(';')[1]?.split('=')[1]
+    console.log(jwt)
+    if (jwt) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+    return {props: {}}
+}
+
 const ResetPassword = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
