02603e5a5294961a981c6f2affcce8c10af673d2
Bahodir8586 <bahodira213@gmail.com>
2021-11-06 14:08:43 +0500

    minor improvements

--- pages/resetPassword/[token].js
+++ pages/resetPassword/[token].js
@@ -1,44 +1,52 @@
-import React, {useState} from 'react';
-import {useRouter} from "next/router";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import ResetPasswordForm from "../../components/ResetPassword";
 import axios from "../../components/axios";
 import Cookies from "js-cookie";
 
 export async function getServerSideProps(context) {
-    const cookies = context.req.headers.cookie?.split('; ').reduce((prevValue, currentValue) => {
-        const key = currentValue.split('=')[0];
-        prevValue[key] = currentValue.split('=')[1];
-        return prevValue;
-    }, {})
-    if (cookies?.token) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false
-            }
-        }
-    }
-    return {props: {}}
+  const cookies = context.req.headers.cookie
+    ?.split("; ")
+    .reduce((prevValue, currentValue) => {
+      const key = currentValue.split("=")[0];
+      prevValue[key] = currentValue.split("=")[1];
+      return prevValue;
+    }, {});
+  if (cookies?.token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
 }
 
 const ResetPassword = () => {
-    const router = useRouter()
-    const [errorMessage, setErrorMessage] = useState("")
-    const resetPasswordHandler = (password, passwordConfirm) => {
-        const token = router.asPath.split("/")[2];
-        console.log(token, password, passwordConfirm)
-        axios.patch(`/users/resetPassword/${token}`, {password, passwordConfirm}).then((response) => {
-            setErrorMessage("")
-            console.log(response)
-            Cookies.set('token', response.data.token)
-            router.push('/')
-        }).catch(error => {
-            setErrorMessage(error.response?.data?.message)
-        })
-    }
-    return (
-        <ResetPasswordForm submitForm={(p, pc) => resetPasswordHandler(p, pc)} errorMessage={errorMessage}/>
-    );
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState("");
+  const resetPasswordHandler = (password, passwordConfirm) => {
+    const token = router.query.token;
+    console.log(token, password, passwordConfirm);
+    axios
+      .patch(`/users/resetPassword/${token}`, { password, passwordConfirm })
+      .then((response) => {
+        setErrorMessage("");
+        console.log(response);
+        Cookies.set("token", response.data.token);
+        router.push("/");
+      })
+      .catch((error) => {
+        setErrorMessage(error.response?.data?.message);
+      });
+  };
+  return (
+    <ResetPasswordForm
+      submitForm={(p, pc) => resetPasswordHandler(p, pc)}
+      errorMessage={errorMessage}
+    />
+  );
 };
 
 export default ResetPassword;
